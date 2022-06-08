5fbe80ed8ac1687b97e6ea055e721bf53a14ade9
vspinni <srkpinni@yahoo.com>
2022-06-08 18:21:22 -0400

    Update App.tsx

--- DuoFinder/App.tsx
+++ DuoFinder/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!</Text>
+      <Text>Enter your League of Legends Username and Select your Server</Text> 
       <StatusBar style="auto" />
     </View>
   );
