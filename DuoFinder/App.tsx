21c2028ff893de0c3e6b7d5f6550f97015149e43
Jiezhi Winston Zuo-yu <71152070+Winzy7@users.noreply.github.com>
2022-06-08 17:30:13 -0500

    textinput

--- DuoFinder/App.tsx
+++ DuoFinder/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import React from "react";
 
 export default function App() {
+  const [text, onChangeText] = React.useState("Enter UserName Here");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!</Text>
+      <TextInput 
+        style = {styles.container}
+        onChangeText = {onChangeText}
+        value = {text}
+      />
       <StatusBar style="auto" />
     </View>
   );
