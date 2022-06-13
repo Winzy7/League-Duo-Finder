76ea04211126bcc113f59264361ecc940347bfe1
Jiezhi Winston Zuo-yu <71152070+Winzy7@users.noreply.github.com>
2022-06-12 19:23:05 -0500

    added navigation library and created first (home) screen

--- DuoFinder/App.tsx
+++ DuoFinder/App.tsx
@@ -1,8 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from "react";
 
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+          name = "Home"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const HomeScreen = ({ navigation }) => {
   const [text, onChangeText] = React.useState("");
   console.log({text});
   return (
@@ -18,8 +36,9 @@ export default function App() {
       />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
 
 const styles = StyleSheet.create({
   container: {
