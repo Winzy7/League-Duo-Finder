addab5a3af86018f8bff31579f91e64f7053d740
Jiezhi Winston Zuo-yu <71152070+Winzy7@users.noreply.github.com>
2022-06-14 21:24:30 -0500

    Update App.tsx
    added some buttons and pressable

--- DuoFinder/App.tsx
+++ DuoFinder/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from "react";
 
 
 const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
@@ -17,27 +18,86 @@ export default function App() {
           <Stack.Screen
           name = "Home"
           component={HomeScreen}
-        />
+          />
+          <Stack.Screen
+          name = "Preferences"
+          component = {Blank}
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+const Blank = ({ navigation }) => {
+  return (
+    <View style = {styles.container}>
+      <Text style = {styles.font}> 
+        Playstyle? 
+      </Text>
+      <View style = {styles.row}>
+          <Pressable
+            onPress= {() => navigation.navigate("Home")}
+            style = {({ pressed }) => [
+              {
+                backgroundColor: pressed
+                ? 'rgb(210, 230, 255)'
+                : 'white'
+              }
+            ]}
+            >
+            {({ pressed }) => (
+              <Text style = {styles.font}>
+                {pressed ? 'Pressed!' : 'Press Me'}
+              </Text>
+              )}
+          </Pressable>
+          <Button 
+            onPress={() => navigation.navigate("Home")}
+            title = "Aggressive"
+            color = "#08B3F3"
+          />
+      </View>
+      <Text style = {styles.font}> 
+        Voice? 
+      </Text>
+      <View style = {styles.row}>
+          <Button
+            onPress={() => navigation.navigate("Home")}
+            title = "Yes"
+            color= "#08B3F3"
+          />
+          <Button 
+            onPress={() => navigation.navigate("Home")}
+            title = "No"
+            color = "#08B3F3"
+          />
+      </View>
+    </View>
+  )
+}
 const HomeScreen = ({ navigation }) => {
   const [text, onChangeText] = React.useState("");
   console.log({text});
   return (
     <View style={styles.container}>
-      <TextInput 
-        style = {styles.box}
-        onChangeText = {onChangeText}
-        value = {text}
-        placeholder = "Enter Username Here"
-        selectionColor = "#08B3F3"
-        autoCapitalize= 'none'
-        textAlign= "center"
-      />
-      <StatusBar style="auto" />
+      <View>
+        <TextInput
+          style = {styles.enterUsername}
+          onChangeText = {onChangeText}
+          value = {text}
+          placeholder = "Enter Username Here"
+          selectionColor = "#08B3F3"
+          autoCapitalize= 'none'
+          textAlign= "center"
+        />
+      </View>
+      <View style = {styles.button1}>
+        <Button
+          onPress = {() => navigation.navigate('Preferences')}
+          title = "Confirm"
+          color = "#08B3F3"
+        />
+      </View>
     </View>
   )
 }
@@ -46,11 +106,12 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  box: {
+  enterUsername: {
     height: 50,
     width: 250,
     top: 60,
@@ -61,4 +122,23 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     fontSize: 17,
   },
+  button1: {
+    top: 80,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  column: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  font: {
+    fontSize: 20,
+    color: "#000000",
+  },
+  press: {
+    fontSize: 20, 
+    color: "#08B3F3"
+  }
 });
